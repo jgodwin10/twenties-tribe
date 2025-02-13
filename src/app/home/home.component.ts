99b15045ed0065975f74bfac6d5527fc8963c2e2
jgodwin10 <easypeasy725@gmail.com>
2025-02-13 09:19:50 +0100

    done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainBodyComponent } from './main-body/main-body.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [NavbarComponent, MainBodyComponent],
+    imports: [NavbarComponent, MainBodyComponent, FooterComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
